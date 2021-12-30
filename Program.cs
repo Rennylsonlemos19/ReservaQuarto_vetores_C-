567994526587615c24167626dc53fe6ebc367692
rennylson-lemos <renylsonlemos4321@gmail.com>
2021-12-30 12:05:59 -0300

    vect

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VECT_reservaQuartos.net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            estudante[] vect = new estudante[10];
+
+            Console.WriteLine("OLÁ QUERIDO CLIENTE");
+            Console.WriteLine();
+            Console.Write("QUANTOS QUARTOS SERÃO ALUGADOS ? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"ALUGUEL #{i}");
+                Console.Write("NOME:");
+                string nome = Console.ReadLine();   
+                Console.Write("E-mail:");
+                string email = Console.ReadLine();
+                Console.Write("QUARTO: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vect[quarto] = new estudante(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("QUARTOS OCUPADOS: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }      
+            Console.ReadKey();
+        }
+    }
+}
---
+++ estudante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VECT_reservaQuartos.net
+{
+    internal class estudante
+    {
+        public string Nome { get; set; }
+        public string Email{ get; set; }
+
+        public estudante(string nome,string email)
+        {
+            this.Nome = nome;
+            this.Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + Email
+                 + ".";
+        }
+    }
+}
